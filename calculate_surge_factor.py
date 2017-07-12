d362f53660b2407940a0dd54a48c28d1fdc1cb53
Charles Martinot <charles.martinot@honestbee.com>
2017-07-12 14:16:37 +0800

    Running in zeppelin

--- calculate_surge_factor.py
+++ calculate_surge_factor.py
@@ -6,33 +6,22 @@
 # geoh_counts_sorted_dstream = geoh_counts.transform((lambda foo:foo.sortBy(lambda x:( -x[1]))))
 # geoh_counts_sorted_dstream.pprint()  
 
+%pyspark
 import json
-import sys
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 
+ssc = StreamingContext(sc, 30)
 
-if __name__ == "__main__":
-  if len(sys.argv) != 3:
-    print("Usage: calculate_surge_factor.py <broker_list> <topic>", file=sys.stderr)
-    exit(-1)
+brokers = "famous-gerbil-kf:9092"
+topic = "pickups"
+kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
 
+pickups = kvs.map(lambda v: json.loads(v[1]))
+pickup_dstream = pickups.map(lambda pickup: pickup['pickup_geohash'])
+geoh_counts = pickup_dstream.countByValue().map(lambda x: (x[0], x[1], float(x[1]/10)/10+1))
+geoh_counts.pprint()
 
-  sc = SparkContext(appName="CalculateSurgeFactor")
-  ssc = StreamingContext(sc, 30)
-
-
-  brokers = sys.argv[1]
-  topic = sys.argv[2]
-  kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
-
-
-  pickups = kvs.map(lambda v: json.loads(v[1]))
-  pickup_dstream = pickups.map(lambda pickup: pickup['pickup_geohash'])
-  geoh_counts = pickup_dstream.countByValue().mapValues(lambda x: x[1])
-  geoh_counts.pprint()
-
-
-  ssc.start()
-  ssc.awaitTermination()
+ssc.start()
+ssc.awaitTermination()
