f8229486efe7f9fef61498b254f1b32812d1a694
Charles Martinot <charles.martinot@honestbee.com>
2017-07-12 14:30:41 +0800

    Send results to kafka

--- calculate_surge_factor.py
+++ calculate_surge_factor.py
@@ -11,17 +11,29 @@ import json
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
+from kafka import SimpleProducer, KafkaClient
+from kafka import KafkaProducer
 
 ssc = StreamingContext(sc, 30)
 
 brokers = "famous-gerbil-kf:9092"
 topic = "pickups"
 kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
+producer = KafkaProducer(bootstrap_servers=brokers)
+
+def handler(message):
+    records = message.collect()
+    for record in records:
+        producer.send('surge', str(record))
+        producer.flush()
 
 pickups = kvs.map(lambda v: json.loads(v[1]))
 pickup_dstream = pickups.map(lambda pickup: pickup['pickup_geohash'])
 geoh_counts = pickup_dstream.countByValue().map(lambda x: (x[0], x[1], float(x[1]/10)/10+1))
 geoh_counts.pprint()
+
+# send results back to kafka
+geoh_counts.foreachRDD(handler)
 
 ssc.start()
 ssc.awaitTermination()
